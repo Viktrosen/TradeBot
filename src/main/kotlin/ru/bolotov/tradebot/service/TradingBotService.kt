d428b57db7e601b30025091597fa78ca523532f0
Viktor Bolotov <bolotov.vit@yandex.ru>
2026-05-05 23:33:37 +0500

    Поправил лимиты

--- src/main/kotlin/ru/bolotov/tradebot/service/TradingBotService.kt
+++ src/main/kotlin/ru/bolotov/tradebot/service/TradingBotService.kt
@@ -156,8 +156,8 @@ class TradingBotService(
                         val portfolio = operationsService.getPortfolioSync(accountId!!)
                         val currentBalance = portfolio.totalAmountCurrencies?.value ?: BigDecimal.ZERO
 
-                        if (currentBalance < BigDecimal.valueOf(10_000_000)) {
-                            val neededAmount = BigDecimal.valueOf(10_000_000)
+                        if (currentBalance < BigDecimal.valueOf(10_000_000_000)) {
+                            val neededAmount = BigDecimal.valueOf(10_000_000_000)
                             sandboxService.payInSync(
                                 accountId!!,
                                 MoneyValue.newBuilder()
